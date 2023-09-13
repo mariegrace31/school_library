b8b593975bcb7f9fbe794c51a9bf2623c52c9f7a
mariegrace <mariegracebmg@gmail.com>
2023-09-13 15:46:45 +0200

    Add the student class

---
+++ student.rb
@@ -0,0 +1,13 @@
+require_realtive 'person'
+class Student < Person
+  attr_accessor :classroom
+
+  def initialize(classroom, age, name: 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    '¯\(ツ)/¯'
+  end
+end
